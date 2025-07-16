ebb11561f87bc32926641b4e04c45e4d64fa5e89
Livius Grosu <liviusgrosu@gmail.com>
2025-07-16 14:48:26 -0600

    Added demo 1

---
+++ Bee Delivery Game/Assets/Scripts/Goal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Goal :  MonoBehaviour
+{
+    public bool IsCurrentGoal;
+    private GameManager _gameManager;
+
+    private void Start()
+    {
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Box") && IsCurrentGoal)
+        {
+            _gameManager.Score();
+            Destroy(collision.gameObject);
+        }
+    }
+}
