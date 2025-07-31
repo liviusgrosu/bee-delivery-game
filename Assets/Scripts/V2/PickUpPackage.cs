e3b393a89defdeca02782ff8caab25b7e815a8af
liviusg <liviusg@nauticsgroup.com>
2025-07-30 22:27:21 -0600

    Added package and simple pickup

---
+++ Assets/Scripts/V2/PickUpPackage.cs
@@ -0,0 +1,60 @@
+﻿
+using UnityEngine;
+
+public class PickUpPackage : MonoBehaviour
+{
+    private Transform _potentialPickUpItem;
+    private Transform _pickedUpItem;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (_potentialPickUpItem && !_pickedUpItem)
+            {
+                BeeAnimation.Instance.PickUp();
+                
+                _pickedUpItem =  _potentialPickUpItem;
+                var packageComponent = _pickedUpItem.GetComponent<Package>();
+                
+                if (packageComponent)
+                {
+                    packageComponent.PickUp();
+                }
+                
+                _pickedUpItem.parent = transform;
+                //_pickedUpItem.position = transform.position;
+            }
+            
+            else if (_pickedUpItem)
+            {
+                BeeAnimation.Instance.DropOff();
+                var package = _pickedUpItem.GetComponent<Package>();
+                
+                if (package)
+                {
+                    package.DropOff();
+                }
+                _pickedUpItem.parent = null;
+                _pickedUpItem = null;
+                _potentialPickUpItem = null;
+            }
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Package"))
+        {
+            _potentialPickUpItem = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Package"))
+        {
+            _potentialPickUpItem = null;
+        }
+    }
+}
